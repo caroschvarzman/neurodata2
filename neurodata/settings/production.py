f7a6decbb51971d078added8ae41b803503c2f3e
Chris <chrisferreyra13@gmail.com>
2020-05-13 00:30:51 -0300

    DB

--- neurodata/settings/production.py
+++ neurodata/settings/production.py
@@ -6,7 +6,11 @@ ALLOWED_HOSTS = ['neurodata.herokuapp.com']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd1q97slu3ksn37',
+        'USER': 'dsetgztaqjgmip',
+        'PASSWORD': '643408f67c1d5158a0eb55a2228547f72ff95dfc3b8be90034ba06e497db1da1',
+        'HOST': 'ec2-54-81-37-115.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
