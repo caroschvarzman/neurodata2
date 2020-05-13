43125e9d83484188b6b0f61e1b017e4acd0800ef
Chris <chrisferreyra13@gmail.com>
2020-05-12 23:51:27 -0300

    inicio

---
+++ neurodata/settings/local.py
@@ -0,0 +1,24 @@
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myDB',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+"""
