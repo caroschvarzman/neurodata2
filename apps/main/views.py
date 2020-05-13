4fd9845588e3ba11423cd83c3eb1757b3d03500d
Chris <chrisferreyra13@gmail.com>
2020-05-13 01:44:00 -0300

    home.html

--- apps/main/views.py
+++ apps/main/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def Hola(request):
-    return HttpResponse('<h1>hola<h1>')
+    return HttpResponse('<h1>Hola insecto<h1>')
