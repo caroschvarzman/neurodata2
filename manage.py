d75c59b2fd96fc8f521ed7eb56db6fd106d7908e
Chris <chrisferreyra13@gmail.com>
2020-05-23 11:34:32 -0300

    detalle

--- manage.py
+++ manage.py
@@ -5,8 +5,8 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'neurodata.settings.local')
-    #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'neurodata.settings.production')
+    #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'neurodata.settings.local')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'neurodata.settings.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- neurodata/wsgi.py
+++ neurodata/wsgi.py
@@ -11,8 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'neurodata.settings.local')
-#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'neurodata.settings.production')
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'neurodata.settings.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'neurodata.settings.production')
 
 application = get_wsgi_application()
 
