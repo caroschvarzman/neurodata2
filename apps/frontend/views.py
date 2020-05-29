fe8456ec3ebbc77c37326946b633863e82506e64
Chris <chrisferreyra13@gmail.com>
2020-05-29 19:38:14 -0300

    prueba de header

--- apps/frontend/views.py
+++ apps/frontend/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'frontend/index.html')
+
+def upload(request):
+    return render(request,'frontend/upload.html')
